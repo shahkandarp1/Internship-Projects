5817c613f0aed8f678aa2acce55d1d493b40746e
shahkandarp1 <kandarp.shah@etatvasoft.com>
2024-01-19 18:56:40 +0530

    currently working on dashboard

--- Final Project/js/patient/form.js
+++ Final Project/js/patient/form.js
@@ -1,29 +1,31 @@
-window.onload = function () {
-    const phoneInputField = document.querySelector("#phone");
-    const phoneInput = window.intlTelInput(phoneInputField, {
-        utilsScript:
-            "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-    });
-}
-
 const getFileData = (myFile) => {
-    var file = myFile.files[0];  
+    var file = myFile.files[0];
     var filename = file.name;
     document.getElementById("form-label").innerHTML = `${filename}`;
 }
 
+$(window).on('load', function () {
+    $('#myModal').modal('show');
+});
+
+$(document).ready(function () {
+    $("#closeBtn").click(function () {
+        $("#myModal").modal("hide");
+    });
+});
+
 const changeMode = () => {
-    try {   
+    try {
         const mode = localStorage.getItem("mode")
-        if(mode==null || mode=="Light"){
-            localStorage.setItem("mode","Dark")
+        if (mode == null || mode == "Light") {
+            localStorage.setItem("mode", "Dark")
             document.getElementById("body").style.backgroundColor = "black";
             document.getElementById("submit-text").style.color = "white"
             document.getElementById("moon").classList.add("hidden")
             document.getElementById("sun").classList.remove("hidden")
         }
-        else{
-            localStorage.setItem("mode","Light")
+        else {
+            localStorage.setItem("mode", "Light")
             document.getElementById("body").style.backgroundColor = "#FAFAFA";
             document.getElementById("submit-text").style.color = "black"
             document.getElementById("sun").classList.add("hidden")
@@ -35,20 +37,28 @@ const changeMode = () => {
 }
 
 const checkMode = (e) => {
-    try {   
+    const phoneInputField = document.getElementsByClassName("phone");
+    for (let i = 0; i < phoneInputField.length; ++i) {
+        const phoneInput = window.intlTelInput(phoneInputField[i], {
+            utilsScript:
+                "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+        });
+    }
+
+    try {
         const mode = localStorage.getItem("mode")
-        if(mode=="Light" || mode==null){
+        if (mode == "Light" || mode == null) {
             document.getElementById("body").style.backgroundColor = "#FAFAFA";
             document.getElementById("submit-text").style.color = "black"
             document.getElementById("sun").classList.add("hidden")
             document.getElementById("moon").classList.remove("hidden")
         }
-        else{
+        else {
             document.getElementById("body").style.backgroundColor = "black";
             document.getElementById("submit-text").style.color = "white"
             document.getElementById("moon").classList.add("hidden")
             document.getElementById("sun").classList.remove("hidden")
-            
+
         }
     } catch (err) {
         alert("there was some issue in changing mode")
