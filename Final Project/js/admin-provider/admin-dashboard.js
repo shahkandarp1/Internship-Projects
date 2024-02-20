f20970a29846d5cd7bc2b5a0109a0f88f4e54d49
shahkandarp1 <kandarp.shah@etatvasoft.com>
2024-02-20 19:55:55 +0530

    did admin dashboard designing

--- Final Project/js/admin-provider/admin-dashboard.js
+++ Final Project/js/admin-provider/admin-dashboard.js
@@ -1,21 +1,41 @@
-function sidebar_open() {
-    if (document.getElementById("mySidebar").offsetWidth == 0) {
-        document.getElementById("mySidebar").style.width = "200px";
-        document.getElementsByClassName("overlay")[0].style.display = "block";
-        document.getElementsByClassName("navbar")[0].style.boxShadow = "none";
+const  changeStatus = (element) => {
+    const states = document.getElementsByClassName("row-cards")
+    const current_state = element.classList[1]
+    for(let i=0;i<states.length;++i){
+        if(states[i].classList.length == 3 && states[i].classList[1] == current_state){
+            break;
+        }
+        else if(states[i].classList[1] == current_state){
+            states[i].classList.add(`${states[i].classList[1]}-active`)
+            document.getElementById(`${states[i].classList[1]}-active`).classList.remove('hidden')
+            document.getElementById(`${states[i].classList[1]}`).classList.add('hidden')
+            document.getElementById(`${states[i].classList[1]}-img`).classList.remove('hidden')
+        }
+        else if(states[i].classList.length == 3)
+        {
+            states[i].classList.remove(`${states[i].classList[1]}-active`)
+            document.getElementById(`${states[i].classList[1]}-active`).classList.add('hidden')
+            document.getElementById(`${states[i].classList[1]}`).classList.remove('hidden')
+            document.getElementById(`${states[i].classList[1]}-img`).classList.add('hidden')
+        }
     }
-    else {
-        document.getElementById("mySidebar").style.width = "0px";
-        document.getElementsByClassName("overlay")[0].style.display = "none";
-        document.getElementsByClassName("navbar")[0].style.boxShadow = "3px -8px 17px 1px black";
-    }
-
+    let name = current_state.split("-").length == 1 ? current_state.split("-")[0] : current_state.split("-")[1]
+    document.getElementById("status-text").innerHTML = `(${name.charAt(0).toUpperCase() + name.slice(1)})`
 }
 
-function sidebar_close() {
-    document.getElementById("mySidebar").style.width = "0px";
-    document.getElementsByClassName("overlay")[0].style.display = "none";
-    for (let i = 0; i < document.getElementsByClassName('accordion-button').length; ++i) {
-        document.getElementsByClassName('accordion-button')[i].style.zIndex = "1";
+const changeRequestor = (element) => {
+    const states = document.getElementsByClassName("req-filter")
+    const current_state = element.classList[1]
+    for(let i=0;i<states.length;++i){
+        if(states[i].classList.length == 3 && states[i].classList[1] == current_state){
+            break;
+        }
+        else if(states[i].classList[1] == current_state){
+            states[i].classList.add(`filter-active`)
+        }
+        else if(states[i].classList.length == 3)
+        {
+            states[i].classList.remove(`filter-active`)
+        }
     }
 }
